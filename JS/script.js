9183a86c2eebdd863b4867af96c4f3ac8c7a7c94
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-09-05 15:27:40 +0200

    added todoList by vue

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,25 @@ console.log('JS OK')
 const { createApp } = Vue
 
 createApp({
-    data() {
-        return {
-        }
-    }
+    data: () => ({
+        toDoList: [
+            { toDo: 'Do my laundry' },
+            { toDo: 'Cancel milk delivery' },
+            { toDo: 'Clean fridge' },
+            { toDo: 'Check passport' },
+            { toDo: 'Do web check-in' },
+            { toDo: 'Download a movie for the flight' },
+            { toDo: 'Recharge mobile' },
+            { toDo: 'Pack swimsuit' },
+            { toDo: 'go biking' },
+            { toDo: 'Laundry' },
+            { toDo: 'cat litter box' },
+            { toDo: 'dishes' },
+            { toDo: 'stretch' },
+            { toDo: 'meditate' },
+        ],
+
+    }),
+
+
 }).mount('#app')
