0ea8d76aa206fdea6704efb58017a996b5728f98
JoseManuel-Feliz <jmgarcia2120@gmail.com>
2024-09-05 22:24:56 +0200

    completed milestone 2

--- JS/script.js
+++ JS/script.js
@@ -73,5 +73,23 @@ createApp({
                 return this.doneClasses = "text-decoration-line-through"
             }
         },
+
+        removeActivity(index) {
+            this.toDoList.splice(index, 1);
+        },
+
+
+        /*  deleteTodoItem(todoItemIndex) {
+             this.todoList.splice(todoItemIndex, 1);
+             console.log(`L'elemento con indice ${todoItemIndex} e' stato rimosso!`);
+         }, */
+
+        /*   deleteTodoIndex(index) {
+              // Your logic to delete the todo item at the given index
+              // For example, you might update a data property or remove an item from an array.
+              this.toDoList.splice(index, 1);
+          } */
+
+
     }
 }).mount('#app')
